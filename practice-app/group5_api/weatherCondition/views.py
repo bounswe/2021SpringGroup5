0f5232d2b2e571e79a8c24cf16974ff3001032d8
Gözde Ünver <gozdeunveer@gmail.com>
2021-05-30 17:09:35 +0300

    validated data before saving to database

--- practice-app/group5_api/weatherCondition/views.py
+++ practice-app/group5_api/weatherCondition/views.py
@@ -10,45 +10,50 @@ def index(request):
     if request.method=='GET':
         return render(request,'weatherCondition/base.html',{})
     else:
-        town=request.POST.get('Town')
-        api_key=settings.WEATHER_KEY
-        api='http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'.format(town,api_key)
-        weathercon = requests.get(api).json()
-        
-        dictionaryNew={
-            "country":weathercon["sys"]["country"], 
-            "town":weathercon["name"], 
-            "x":weathercon["coord"]["lat"],
-            "y":weathercon["coord"]["lon"],
-            "description":weathercon["weather"][0]["description"],
-            "degrees":weathercon["main"]["temp"], 
-            "pressure":weathercon["main"]["pressure"],
-            "humidity":weathercon["main"]["humidity"],
-            "speed":weathercon["wind"]["speed"], 
-            "degreeOfWind":weathercon["wind"]["deg"]
-            }
-        
         try:
-            dictionaryOLD=WeatherConditionSerializer(WeatherCondition.objects.order_by('id')[0])
+            dictionaryOLD=WeatherConditionSerializer(WeatherCondition.objects.order_by('-id')[0])
         except:
             dictionaryOLD={}
-
-        newWeather=WeatherCondition(country=dictionaryNew["country"],
-        town=dictionaryNew["town"],
-        x=dictionaryNew["x"],
-        y=dictionaryNew["y"],
-        description=dictionaryNew["description"],
-        degrees=dictionaryNew["degrees"],
-        pressure=dictionaryNew["pressure"],
-        humidity=dictionaryNew["humidity"],
-        speed=dictionaryNew["speed"],
-        degreeOfWind=dictionaryNew["degreeOfWind"])
-        
-        newWeather.save()
         try:
             oldData=dictionaryOLD.data
         except:
             oldData=False
+        town=request.POST.get('Town')
+        api_key=settings.WEATHER_KEY
+        api='http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'.format(town,api_key)
+        weathercon= requests.get(api).json()
+        if weathercon["cod"]!="404":
+            dictionaryNew={
+                "country":weathercon["sys"]["country"], 
+                "town":weathercon["name"], 
+                "x":weathercon["coord"]["lat"],
+                "y":weathercon["coord"]["lon"],
+                "description":weathercon["weather"][0]["description"],
+                "degrees":weathercon["main"]["temp"], 
+                "pressure":weathercon["main"]["pressure"],
+                "humidity":weathercon["main"]["humidity"],
+                "speed":weathercon["wind"]["speed"], 
+                "degreeOfWind":weathercon["wind"]["deg"]
+                }
+            newWeather=WeatherCondition(country=dictionaryNew["country"],
+            town=dictionaryNew["town"],
+            x=dictionaryNew["x"],
+            y=dictionaryNew["y"],
+            description=dictionaryNew["description"],
+            degrees=dictionaryNew["degrees"],
+            pressure=dictionaryNew["pressure"],
+            humidity=dictionaryNew["humidity"],
+            speed=dictionaryNew["speed"],
+            degreeOfWind=dictionaryNew["degreeOfWind"])
+
+            temp=WeatherConditionSerializer(data=dictionaryNew)
+            if temp.is_valid():
+                dictionaryNew=temp.validated_data
+                newWeather.save()
+            else:
+                dictionaryNew={}
+        else:
+            dictionaryNew={}
 
         dictionary={"new":dictionaryNew,"old":oldData}
         
