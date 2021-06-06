2ff66f5f2980662997c561c65749decbfe3f12d3
Oozankilic <81261090+Oozankilic@users.noreply.github.com>
2021-06-06 14:26:14 +0300

    Update settings.py
    installed apps is updated

--- practice-app/group5_api/group5_api/settings.py
+++ practice-app/group5_api/group5_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'randomActivity.apps.RandomactivityConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
