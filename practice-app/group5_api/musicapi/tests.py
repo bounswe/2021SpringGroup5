ec636ebf69b358e1190d748749ab68e221dd763b
cagin24 <47904355+cagin24@users.noreply.github.com>
2021-06-07 21:37:51 +0300

    Update tests.py

--- practice-app/group5_api/musicapi/tests.py
+++ practice-app/group5_api/musicapi/tests.py
@@ -2,12 +2,9 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
-
-# Create your tests here.
-
 class musicapi(APITestCase):
 
-    # Tests if the get request works
+    # Test GET method
     def test_get_method(self):
         client = APIClient()
         response = client.get("/musicapi/api/")
