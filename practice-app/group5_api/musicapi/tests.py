e57c9a969e9f38f48457bb6e5bbb755e8616a6a9
cagin24 <47904355+cagin24@users.noreply.github.com>
2021-06-07 21:37:24 +0300

    Update tests.py

--- practice-app/group5_api/musicapi/tests.py
+++ practice-app/group5_api/musicapi/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+
 
 # Create your tests here.
+
+class musicapi(APITestCase):
+
+    # Tests if the get request works
+    def test_get_method(self):
+        client = APIClient()
+        response = client.get("/musicapi/api/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
