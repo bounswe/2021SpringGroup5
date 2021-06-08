79d6875e9efbe1f939f26e4f25782ba26567a53b
atasoyfurkan <46852761+atasoyfurkan@users.noreply.github.com>
2021-06-08 16:39:18 +0300

    Update urls.py

--- practice-app/group5_api/home/urls.py
+++ practice-app/group5_api/home/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('sportRelation/', include('sport_relation.urls'))
+    path('sportRelation/', include('sport_relation.urls')),
     path('dailyQuote/', include('dailyQuote.urls')),
     path('weatherCondition/',include('weatherCondition.urls')),
     path('nbaStats/', include('nbaStats.urls'))
