8806e9a712c6f73098fa80f056f19723f471a1c4
e7hdk <mehmet.fatih.aksoy@hotmail.com>
2021-06-09 01:14:19 +0300

    Merged fatibranch

---
+++ practice-app/group5_api/core/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+# Create your views here.
+def home(request):
+	count = User.objects.count()
+	return render(request, 'home.html', {
+		'count': count
+		})
+
+def signup(request):
+	if request.method == 'POST':
+
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('home')
+	else:
+		form = UserCreationForm()
+	return render(request, 'signup.html', {
+		'form':form
+		})
