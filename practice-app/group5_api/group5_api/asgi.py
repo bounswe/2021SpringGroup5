e1b3d239eeeadd888c9b54d5b3079bbc5a15a24e
Berkay Bör <berkay.bor07@gmail.com>
2021-06-08 22:27:31 +0300

    Revert "Merge branch 'berkaybranch'"
    This reverts commit bb46b3382af7235a259e5ffe1ac077b6bfed2979, reversing
    changes made to 01ba3f5c4ec6917746adc1a2c06e52db10466b18.

--- practice-app/group5_api/group5_api/asgi.py
+++ practice-app/group5_api/group5_api/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for group5-api project.
+ASGI config for group5_api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'group5-api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'group5_api.settings')
 
 application = get_asgi_application()
--- practice-app/home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
-
-
-@api_view(['GET'])
-def index(request):
-    return render(request,'home/home.html',{})
-
