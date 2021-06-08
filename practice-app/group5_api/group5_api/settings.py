f8231ed133dc73b31ca0cee5ad81ee2588de7189
Umut Gün <ug@Umuts-MacBook-Air.local>
2021-06-08 20:08:35 +0300

    fixed settings

--- practice-app/group5_api/group5_api/settings.py
+++ practice-app/group5_api/group5_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-28a9ude6ep&ic*dwcr(jfqc7l%q6@jb1lb78z4ebdjgpj)55sl'
+SECRET_KEY = decouple.config('SECRET_KEY', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'exchangeRateAPI'
 ]
 
 MIDDLEWARE = [
