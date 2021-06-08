86625dc9715b8c6d15a93c6130fcba52c1e2e96c
Umut Gün <ug@Umuts-MacBook-Air.local>
2021-06-08 20:09:50 +0300

    fixed settings2

--- practice-app/group5_api/home/urls.py
+++ practice-app/group5_api/home/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sportRelation/', include('sport_relation.urls')),
+    path('dailyQuote/', include('dailyQuote.urls')),
+    path('weatherCondition/',include('weatherCondition.urls')),
+    path('nbaStats/', include('nbaStats.urls')),
+    path('musicapi/',include('musicapi.urls'))
 ]
