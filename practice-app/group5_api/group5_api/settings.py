c47fd7ed8f02c5316b0508392e29e7ed42f15b22
Berkay Bör <berkay.bor07@gmail.com>
2021-06-09 19:57:58 +0300

    modified allowed hosts

--- practice-app/group5_api/group5_api/settings.py
+++ practice-app/group5_api/group5_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = decouple.config('SECRET_KEY', default='')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
