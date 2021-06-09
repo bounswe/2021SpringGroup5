2988d1cf0b8ffb620ba70d2b151fabc84ece8e44
Berkay Bör <berkay.bor07@gmail.com>
2021-06-10 00:29:58 +0300

    fixed tests

--- practice-app/group5_api/airqualityapi/tests.py
+++ practice-app/group5_api/airqualityapi/tests.py
@@ -9,30 +9,30 @@ class AirQualityTests(APITestCase):
     def test_invalid_user_id(self):
         data = {'user_id': 'asd'}
         client = APIClient()
-        response = client.get('airQuality/api/pollution/', data)
+        response = client.get('/airQuality/api/pollution/', data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     # Tests if a user id is not found in location history
     def test_user_id_not_found(self):
         data = {'user_id': '-1'}
         client = APIClient()
-        response = client.get('airQuality/api/pollution/', data)
+        response = client.get('/airQuality/api/pollution/', data)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     # Tests adding a location and
     def test_add_location(self):
         data = {'user_id': '5', 'latitude': '39.7667', 'longitude': '30.5256'}
         client = APIClient()
-        response = client.post('airQuality/api/locations/', data)
+        response = client.post('/airQuality/api/locations/', data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     # Creates a location and retrives it
     def test_get_location(self):
         client = APIClient()
         data = {'user_id': '5', 'latitude': '39.7667', 'longitude': '30.5256'}
-        client.post('/api/locations/', data)
+        client.post('/airQuality/api/locations/', data)
         data = {'user_id': '5'}
-        response = client.get('airQuality/api/locations/', data)
+        response = client.get('/airQuality/api/locations/', data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(list(response.data[0].keys()), ['created', 'user_id', 'latitude', 'longitude'])
 
@@ -40,8 +40,8 @@ class AirQualityTests(APITestCase):
     def test_get_latest_pollution_of_user_location(self):
         client = APIClient()
         data = {'user_id': '5', 'latitude': '39.7667', 'longitude': '30.5256'}
-        client.post('airQuality/api/locations/', data)
+        client.post('/airQuality/api/locations/', data)
         data = {'user_id': '5'}
-        response = client.get('/api/pollution/', data)
+        response = client.get('/airQuality/api/pollution/', data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(list(response.data.keys()), ['aqi', 'name', 'color', 'description', 'polluiton_ts', 'city', 'state', 'country', 'coordinates'])
