037bea9966462c97ab4ecaad4bc2ba65480f9737
e7hdk <47904054+e7hdk@users.noreply.github.com>
2021-11-02 14:21:07 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,11 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-<<<<<<< Updated upstream
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Authentication.settings')
-=======
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
->>>>>>> Stashed changes
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
