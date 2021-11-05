55604ca2868e1dfe920e0bf3091791bd55540969
gozde <gozdeunveer@gmail.com>
2021-11-05 16:04:14 +0300

    settings file is changed so that the database is connected and used for the docker

--- project/backend/app/app/settings.py
+++ project/backend/app/app/settings.py
@@ -75,9 +75,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'group5db',
+        'USER': 'postgres',
+        'PASSWORD': 'group5',
+        'HOST': 'db',
+        'PORT': 5432,
     }
+
 }
 
 
