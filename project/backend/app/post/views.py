f1861176f44cfac20e70bae749e882c1eebb447b
gozde <gozdeunveer@gmail.com>
2021-11-06 17:33:32 +0300

    changed badge model and created BadgeOwnedByUser model

--- project/backend/app/post/views.py
+++ project/backend/app/post/views.py
@@ -5,14 +5,24 @@ import requests
 from django.conf import settings
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth.decorators import login_required
+import datetime
 
 # Create your views here.
-
+@login_required(login_url='login_user/')
 @api_view(['POST'])
 def createEventPost(request):
     data=request.POST
     _,name,surname,username,id=data["actor"]
-    _,kokok=data["object"]
+    
+    _,owner_id,name,created_date,sport_category,location,description,\
+    image,date_time,participant_limit,spectator_limit,rule,equipment_requirement, \
+    status, capacity, location_requirement, contact_info, skill_requirement,repeating_frequency,badges,applications =data["object"]
+    
+    #Check if the date_time is valid
+    #date = datetime.datetime.strptime(date_time, "%Y-%m-%d %H:%M")
+    #date.day)   
+     
     # İlgili event post verileir kullanılarak ve if ser.is_valid(): kullandıktan sonra dbye kaydet,
     # Offered badges'a da bir entry girilecek "badges": [ "inadequate_skill","good_teacher","friendly"] ve user_id kullanarak
 
