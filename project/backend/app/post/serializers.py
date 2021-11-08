fc67d1296c5d0ff5ebcad87e23d487f512c9409f
gozde <gozdeunveer@gmail.com>
2021-11-08 12:56:57 +0300

    added missing serializers of the models (Event and equipment post activity stream, event and equipment comment, badge owned by user and skill level. Changed the fields of the sport serializer to __all__ because the fields of the sport model is changed

--- project/backend/app/post/serializers.py
+++ project/backend/app/post/serializers.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.db.models import fields
 from rest_framework import serializers
-from post.models import Sport,EquipmentPost,EventPost,Application,Badge,BadgeOfferedByEventPost
+from post.models import BadgeOwnedByUser, EquipmentComment, EquipmentCommentActivtyStream, EventComment,\
+    EventCommentActivtyStream, SkillLevel, Sport,EquipmentPost,EventPost,Application,Badge,BadgeOfferedByEventPost,EventPostActivityStream,\
+    EquipmentPostActivtyStream
+
 
 class SportSerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,11 +21,44 @@ class EventPostSerializer(serializers.ModelSerializer):
         model=EventPost
         fields='__all__'
 
+class SkillLevelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=SkillLevel
+        fields='__all__'
 
+class EventPostActivityStreamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=EventPostActivityStream
+        fields='__all__'
+
+class EquipmentPostActivtyStreamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=EquipmentPostActivtyStream
+        fields='__all__'
 
 class ApplicationSerializer(serializers.ModelSerializer):
     class Meta:
         model=Application
+        fields='__all__'
+
+class EventCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=EventComment
+        fields='__all__'
+
+class EquipmentCommentActivityStreamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=EquipmentCommentActivtyStream
+        fields='__all__'
+
+class EventCommentActivityStreamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=EventCommentActivtyStream
+        fields='__all__'
+
+class EquipmentCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=EquipmentComment
         fields='__all__'
 
 class BadgeSerializer(serializers.ModelSerializer):
@@ -33,4 +69,9 @@ class BadgeSerializer(serializers.ModelSerializer):
 class BadgeOfferedByEventPostSerializer(serializers.ModelSerializer):
     class Meta:
         model=BadgeOfferedByEventPost
+        fields='__all__'
+
+class BadgeOwnedByUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=BadgeOwnedByUser
         fields='__all__'
