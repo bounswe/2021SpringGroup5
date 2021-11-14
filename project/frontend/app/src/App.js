6139967cb1f087c3250025473e50f658e221b373
zudiay <zdaytac@gmail.com>
2021-11-14 17:51:16 +0300

    Added forgot password route. Added function to AuthService. Finished ForgotPasswordScreen with responsive styles and alerts using LandingPage.css

--- project/frontend/app/src/App.js
+++ project/frontend/app/src/App.js
@@ -8,37 +8,41 @@ import EventScreen from './screens/EventScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
 
 function App() {
-    const [user, setUser] = useState();
-    const queryClient = new QueryClient();
+  const [user, setUser] = useState();
+  const queryClient = new QueryClient();
 
-    return (
-        <div className="App">
-            <QueryClientProvider client={queryClient}>
-                {user && (
-                    <>
-                        <Header user={user} />
-                        <Switch>
-                            <Route path="/event/:id" component={EventScreen} />
-                            <Route path="/" component={HomeScreen} />
-                        </Switch>
-                    </>
-                )}
-                {!user && (
-                    <Switch>
-                        <Route path="/login">
-                            <LoginScreen setUser={setUser} />
-                        </Route>
-                        <Route path="/register">
-                            <RegisterScreen />
-                        </Route>
-                        <Redirect exact from="/" to="/login" />
-                    </Switch>
-                )}
-            </QueryClientProvider>
-        </div>
-    );
+  return (
+    <div className="App">
+      <QueryClientProvider client={queryClient}>
+        {user && (
+          <>
+            <Header user={user} />
+            <Switch>
+              <Route path="/event/:id" component={EventScreen} />
+              <Route path="/" component={HomeScreen} />
+            </Switch>
+          </>
+        )}
+        {!user && (
+          <Switch>
+            <Route path="/login">
+              <LoginScreen setUser={setUser} />
+            </Route>
+            <Route path="/forgot-password">
+              <ForgotPasswordScreen />
+            </Route>
+            <Route path="/register">
+              <RegisterScreen />
+            </Route>
+            <Redirect exact from="/" to="/login" />
+          </Switch>
+        )}
+      </QueryClientProvider>
+    </div>
+  );
 }
 
 export default App;
--- project/frontend/app/src/services/AuthService.js
+++ project/frontend/app/src/services/AuthService.js
@@ -1,10 +1,16 @@
 import { httpClient } from '../httpClient';
 
 export function login(loginForm) {
-    return httpClient.post('/login', {
-        actor: {
-            type: 'Person',
-            ...loginForm
-        },
-    });
+  return httpClient.post('/login', {
+    actor: {
+      type: 'Person',
+      ...loginForm,
+    },
+  });
+}
+
+export function forgotPassword(forgotPasswordForm) {
+  return httpClient.post('/forgot-password', {
+    ...forgotPasswordForm,
+  });
 }
