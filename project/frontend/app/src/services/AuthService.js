f06104dc2feeee1f9cff9d4fce0d1709ea9835b9
zudiay <zdaytac@gmail.com>
2021-11-15 12:33:33 +0300

    added /node_modules to .gitignore added register function to AuthService.js that maps the form to backend schema added helpers folder and function.js that includes toTitleCase added new style to LandingPage.css added RegisterScreen.jsx with 2-step register form added SportsService.js and mock for sports and skill levels

--- project/frontend/app/src/services/AuthService.js
+++ project/frontend/app/src/services/AuthService.js
@@ -14,3 +14,27 @@ export function forgotPassword(forgotPasswordForm) {
     ...forgotPasswordForm,
   });
 }
+
+export function register(registerForm) {
+  return httpClient.post('/register', {
+    actor: {
+      type: 'Person',
+      username: registerForm.username,
+      email: registerForm.email,
+      name: registerForm.name,
+      surname: registerForm.surname,
+      password1: registerForm.password,
+      password2: registerForm.password_confirm,
+      items: [
+        {
+          name: registerForm.sport_1,
+          level: registerForm.level_1,
+        },
+        {
+          name: registerForm.sport_2,
+          level: registerForm.level_2,
+        },
+      ],
+    },
+  });
+}
