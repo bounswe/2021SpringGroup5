63a989f6ea1de543911ca3852ba5664564efd908
cagin24 <cagin24@gmail.com>
2021-12-11 22:21:33 +0300

    add search bar on event list screen

--- project/android/app/lib/screens/event_list_page/event_list_screen.dart
+++ project/android/app/lib/screens/event_list_page/event_list_screen.dart
@@ -5,21 +5,44 @@ import 'package:ludo_app/screens/event_list_page/event_item.dart';
 //in this page the events will be displayed from top to bottom, if tap on a event card it will open the
 // event card details page
 
-class EventListScreen extends StatelessWidget {
+class EventListScreen extends StatefulWidget {
+  @override
+  _EventListScreenState createState() => _EventListScreenState();
+}
+
+class _EventListScreenState extends State<EventListScreen> {
   final eventList = Event.generateEvent();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration:
-            BoxDecoration(color: Colors.lightGreenAccent.withOpacity(0.6)),
-        child: ListView.separated(
-            padding: EdgeInsets.symmetric(horizontal: 15),
-            scrollDirection: Axis.vertical,
-            itemBuilder: (context, index) => EventItem(eventList[index]),
-            separatorBuilder: (_, index) => SizedBox(
-                  height: 20,
+    return Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backwardsCompatibility: true,
+          title: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.050,
+            color: Colors.white,
+            child: Center(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search for an event',
+                  prefixIcon: Icon(Icons.search),
                 ),
-            itemCount: eventList.length));
+              ),
+            ),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: ListView.separated(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              scrollDirection: Axis.vertical,
+              itemBuilder: (context, index) => EventItem(eventList[index]),
+              separatorBuilder: (_, index) => SizedBox(
+                    height: 20,
+                  ),
+              itemCount: eventList.length),
+        ));
   }
 }
