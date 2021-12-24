31bf2f7ed414239d3af0c3ed082863199e925a06
salih997 <56520923+salih997@users.noreply.github.com>
2021-12-25 01:23:41 +0300

    Update serializers.py
    I deleted an unnecessary serializer.

--- project/backend/app/post/serializers.py
+++ project/backend/app/post/serializers.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models import fields
 from rest_framework import serializers
 from post.models import Sport,EquipmentPost,EventPost,EventComment,Application,Badge,BadgeOfferedByEventPost, \
-    EquipmentComment, SkillLevel, BadgeOwnedByUser, EventPostActivityStream,EquipmentPostActivtyStream, ApplicationActivityStream
+    EquipmentComment, SkillLevel, BadgeOwnedByUser, EventPostActivityStream,EquipmentPostActivtyStream
 
 class SportSerializer(serializers.ModelSerializer):
     class Meta:
@@ -63,9 +63,4 @@ class BadgeOfferedByEventPostSerializer(serializers.ModelSerializer):
 class BadgeOwnedByUserSerializer(serializers.ModelSerializer):
     class Meta:
         model=BadgeOwnedByUser
-        fields='__all__'
-
-class ApplicationActivityStreamSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=ApplicationActivityStream
         fields='__all__'
