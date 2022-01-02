59613a918b9c60228d7a5c96a5224ff3966bce2b
Berkay Bör <berkay.bor07@gmail.com>
2022-01-02 20:55:45 +0300

    Implemented create event and connected it to backend

--- project/android/app/lib/screens/create_event/create_event_screen.dart
+++ project/android/app/lib/screens/create_event/create_event_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:ludo_app/screens/google_maps/google_maps_screen_no_radius.dart';
 import 'package:ludo_app/screens/main_events/main_event_screen.dart';
+import 'dart:io';
+import 'package:ludo_app/globals.dart' as globals;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart' as intl;
 
 class CreateEventScreen extends StatefulWidget {
   const CreateEventScreen({Key? key}) : super(key: key);
@@ -16,11 +22,195 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
   bool _skilledFlag = true;
   bool _expertFlag = true;
   List<double> mapCallback = [];
+  late ImagePicker imagePicker;
+  var _image;
+  DateTime selectedDateTime = DateTime.now();
 
-  _updateLocation(List<double> maps){
+  final eventNameController = TextEditingController();
+  final eventDescriptionController = TextEditingController();
+  final sportNameController = TextEditingController();
+  final participantLimitController = TextEditingController();
+  final spectatorLimitController = TextEditingController();
+  final rulesController = TextEditingController();
+  final equipmentRequirementController = TextEditingController();
+  final contactInfoController = TextEditingController();
+
+  _updateLocation(List<double> maps) {
     setState(() {
       mapCallback = maps;
     });
+  }
+
+  showAlertDialog(BuildContext context) async {
+    Future<String>? _futureResponse;
+    FutureBuilder<String> buildFutureBuilder() {
+      return FutureBuilder<String>(
+        future: _futureResponse,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Text(snapshot.data!);
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+
+          return const CircularProgressIndicator();
+        },
+      );
+    }
+
+    _futureResponse = createEvent(context);
+    // late var futureRegister = fetchRegister();
+    // print(futureRegister);
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      content: buildFutureBuilder(),
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  Future<String> createEvent(BuildContext context) async {
+    String skillLevel = "beginner";
+    if (_beginnerFlag == false) {
+      skillLevel = "beginner";
+    } else if (_averageFlag == false) {
+      skillLevel = "average";
+    } else if (_skilledFlag == false) {
+      skillLevel = "skilled";
+    } else if (_expertFlag == false) {
+      skillLevel = "expert";
+    }
+    var params = {
+      "@context": "https://www.w3.org/ns/activitystreams",
+      "summary": "${globals.name} is creating an event post",
+      "type": "Create",
+      "actor": {
+        "type": "Person",
+        "name": globals.name,
+        "surname": globals.surname,
+        "username": globals.username,
+        "Id": globals.userid,
+      },
+      "object": {
+        "type": "EventPost",
+        "owner_id": globals.userid,
+        "post_name": eventNameController.text,
+        "sport_category": sportNameController.text,
+        "longitude": mapCallback[0],
+        "latitude": mapCallback[1],
+        "description": eventDescriptionController.text,
+        "pathToEventImage": null,
+        "date_time":
+            intl.DateFormat("yyyy-MM-dd hh:mm").format(selectedDateTime),
+        "participant_limit": int.parse(participantLimitController.text),
+        "spectator_limit": int.parse(spectatorLimitController.text),
+        "rule": rulesController.text,
+        "equipment_requirement": equipmentRequirementController.text,
+        "location_requirement": "",
+        "contact_info": contactInfoController.text,
+        "skill_requirement": skillLevel,
+        "repeating_frequency": 1,
+        "badges": [
+          {
+            "id": 1,
+            "name": "friendly",
+            "description": "You are a friendly player",
+            "wikiId": ""
+          }
+        ]
+      }
+    };
+
+    var uri = Uri.parse('http://3.122.41.188:8000/post/create_event_post/');
+    var request = http.MultipartRequest('POST', uri);
+    request.headers.addAll(
+      <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authentication': globals.access,
+        'X-CSRFTOKEN': globals.csrftoken,
+        'Cookie':
+            'csrftoken=${globals.csrftoken}; sessionid=${globals.sessionid}'
+      },
+    );
+    request.files.add(await http.MultipartFile.fromPath('image', _image));
+    request.fields['json'] = jsonEncode(params);
+    var response = await request.send();
+
+    print(response.statusCode);
+    print(response.stream.toString());
+    if (response.statusCode == 201) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return MainEventScreen(willFetchAllEvents: true);
+          },
+        ),
+      );
+      return response.toString();
+    } else {
+      throw Exception(response.stream.toString());
+    }
+  }
+
+  void _showPicker(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Container(
+              child: Wrap(
+                children: <Widget>[
+                  ListTile(
+                      leading: const Icon(Icons.photo_library),
+                      title: const Text('Photo Library'),
+                      onTap: () {
+                        _imgFromGallery();
+                        Navigator.of(context).pop();
+                      }),
+                  ListTile(
+                    leading: const Icon(Icons.photo_camera),
+                    title: const Text('Camera'),
+                    onTap: () {
+                      _imgFromCamera();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  _imgFromCamera() async {
+    final XFile? image = await imagePicker.pickImage(
+        source: ImageSource.camera,
+        imageQuality: 50,
+        preferredCameraDevice: CameraDevice.rear);
+    setState(() {
+      _image = image!.path;
+    });
+  }
+
+  _imgFromGallery() async {
+    final XFile? image = await imagePicker.pickImage(
+        source: ImageSource.gallery, imageQuality: 50);
+    setState(() {
+      _image = image!.path;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    imagePicker = ImagePicker();
   }
 
   @override
@@ -33,6 +223,36 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
           padding: const EdgeInsets.all(5.0),
           child: Column(
             children: [
+              const SizedBox(height: 15, width: 10),
+              Center(
+                child: GestureDetector(
+                  onTap: () {
+                    _showPicker(context);
+                  },
+                  child: Container(
+                    width: 320,
+                    height: 180,
+                    decoration: BoxDecoration(color: Colors.white),
+                    child: _image != null
+                        ? Image.file(
+                            File(_image),
+                            width: 280.0,
+                            height: 210.0,
+                            fit: BoxFit.fitHeight,
+                          )
+                        : Container(
+                            decoration: BoxDecoration(color: Colors.grey[100]),
+                            width: 280,
+                            height: 210,
+                            child: Icon(
+                              Icons.camera_alt,
+                              color: Colors.grey[800],
+                            ),
+                          ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10, width: 10),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Row(
@@ -47,15 +267,17 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
               ),
               Container(
                   child: TextFormField(
-                decoration: InputDecoration(
-                  hintText: ('Type here...'),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide(),
-                  ),
-                ),
-                keyboardType: TextInputType.text,
-              )),
+                      decoration: InputDecoration(
+                      hintText: ('Type here...'),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(),
+                      ),
+                    ),
+                    keyboardType: TextInputType.text,
+                    controller: eventNameController,
+                  )
+              ),
 
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -72,15 +294,45 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
 
               Container(
                   child: TextFormField(
-                decoration: InputDecoration(
-                  hintText: ('Type here...'),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide(),
-                  ),
+                    decoration: InputDecoration(
+                      hintText: ('Type here...'),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(),
+                      ),
+                    ),
+                    keyboardType: TextInputType.text,
+                    controller: eventDescriptionController,
+                  )
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Container(
+                        decoration:
+                            BoxDecoration(color: Colors.blue.withOpacity(0.4)),
+                        child: const Text("Enter Sport Name",
+                            style: TextStyle(fontSize: 16)))
+                  ],
                 ),
-                keyboardType: TextInputType.text,
-              )),
+              ),
+
+              Container(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: ('e.g. tennis'),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(),
+                      ),
+                    ),
+                    keyboardType: TextInputType.text,
+                    controller: sportNameController,
+                  )
+              ),
+
               Padding(
                 padding: const EdgeInsets.only(
                     left: 8.0, right: 8.0, top: 6.0, bottom: 8.0),
@@ -184,7 +436,8 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         SizedBox(
-                          child: GoogleMapsNoRadiusScreen(parentAction: _updateLocation),
+                          child: GoogleMapsNoRadiusScreen(
+                              parentAction: _updateLocation),
                           height: MediaQuery.of(context).size.height * 0.59,
                           width: MediaQuery.of(context).size.width * 0.93,
                         ),
@@ -213,32 +466,155 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
                 height: 200,
                 child: CupertinoDatePicker(
                   mode: CupertinoDatePickerMode.dateAndTime,
-                  initialDateTime: DateTime(2021, 12, 1, 12, 00),
+                  initialDateTime: DateTime.now(),
                   onDateTimeChanged: (DateTime newDateTime) {
-                    //Do Some thing
+                    setState(() {
+                      selectedDateTime = newDateTime;
+                    });
                   },
                   use24hFormat: false,
                   minuteInterval: 1,
                 ),
               ),
 
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Container(
+                        decoration:
+                            BoxDecoration(color: Colors.blue.withOpacity(0.4)),
+                        child: const Text("Participant Limit",
+                            style: TextStyle(fontSize: 16)))
+                  ],
+                ),
+              ),
+              Container(
+                  child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: ('e.g. 5'),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(),
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+                controller: participantLimitController,
+              )),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Container(
+                        decoration:
+                            BoxDecoration(color: Colors.blue.withOpacity(0.4)),
+                        child: const Text("Spectator Limit",
+                            style: TextStyle(fontSize: 16)))
+                  ],
+                ),
+              ),
+              Container(
+                  child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: ('e.g. 2'),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(),
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+                controller: spectatorLimitController,
+              )),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Container(
+                        decoration:
+                            BoxDecoration(color: Colors.blue.withOpacity(0.4)),
+                        child: const Text("Enter Event Rules",
+                            style: TextStyle(fontSize: 16)))
+                  ],
+                ),
+              ),
+              Container(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: ('e.g. "Be kind!"'),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(),
+                      ),
+                    ),
+                    keyboardType: TextInputType.text,
+                    controller: rulesController,
+                  )
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Container(
+                        decoration:
+                        BoxDecoration(color: Colors.blue.withOpacity(0.4)),
+                        child: const Text("Enter Any Equipment Requirements",
+                            style: TextStyle(fontSize: 16)))
+                  ],
+                ),
+              ),
+              Container(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: ('e.g. "tennis racket"'),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(),
+                      ),
+                    ),
+                    keyboardType: TextInputType.text,
+                    controller: equipmentRequirementController,
+                  )
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Container(
+                        decoration:
+                        BoxDecoration(color: Colors.blue.withOpacity(0.4)),
+                        child: const Text("Enter Contact Info",
+                            style: TextStyle(fontSize: 16)))
+                  ],
+                ),
+              ),
+              Container(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: ('05xxxxxxxxx'),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(),
+                      ),
+                    ),
+                    keyboardType: TextInputType.phone,
+                    controller: contactInfoController,
+                  )
+              ),
+              const SizedBox(height: 15, width: 10),
               ElevatedButton(
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return const MainEventScreen();
-                      },
-                    ),
-                  );
+                  showAlertDialog(context);
                 },
                 child: const Text(
                   "CREATE",
                   style: TextStyle(fontSize: 16),
                 ),
               ), //map
-              Text(mapCallback.toString()),
+              const SizedBox(height: 20, width: 10),
             ],
           ),
         ));
