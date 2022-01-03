35072feaa514f86a636231b7693df25078d11235
atasoyfurkan <furkanatasoy65@gmail.com>
2022-01-03 20:28:36 +0300

    Add Unit Test to Home Screen

---
+++ project/frontend/app/src/__tests__/homeScreen.test.js
@@ -0,0 +1,19 @@
+import HomeScreen from '../screens/HomeScreen.js';
+import { render as rtlRender, screen, waitFor } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import userEvent from '@testing-library/user-event';
+
+function render(children) {
+    const queryClient = new QueryClient();
+    return rtlRender(<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>);
+}
+
+test('check that if there is any post from followed users it is shown correctly or not', async () => {
+    render(<HomeScreen />);
+    expect(screen.getByTestId(/followedPost/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /navigatePostDetails/i })).toBeInTheDocument();
+    const event = screen.getByRole('button', { name: /navigatePostDetails/i });
+
+    userEvent.click(event);
+    await waitFor(() => { });
+});
